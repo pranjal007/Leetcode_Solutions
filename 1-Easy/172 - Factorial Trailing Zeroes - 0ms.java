c08aa76f7d57efa7c9022515da357e2454aa4165
Pranjal <bahuguna.pranjal@gmail.com>
2020-07-27 23:06:02 -0400

    27 July 2020

---
+++ 1-Easy/172 - Factorial Trailing Zeroes - 0ms.java
@@ -0,0 +1,17 @@
+
+// https://leetcode.com/problems/factorial-trailing-zeroes/
+
+class Solution {
+    public int trailingZeroes(int n) {
+                
+        int count=0;
+        while(n!=0){
+            
+            int temp = n/5;
+            count +=temp;
+            n = temp;
+        }
+        return count;
+        
+    }
+}

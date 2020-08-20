3faa68b4c6bf4da1e2e0cbcc7671a3546529f245
Pranjal <bahuguna.pranjal@gmail.com>
2020-08-19 23:33:27 -0400

    19 Aug 2020

---
+++ 1-Easy/226 - Invert Binary Tree - 0ms.java
@@ -0,0 +1,20 @@
+
+// https://leetcode.com/problems/invert-binary-tree/
+
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        
+        
+        if(null == root){
+            return null;
+        }
+        
+        TreeNode left = invertTree(root.left);
+        TreeNode right = invertTree(root.right);
+        root.left = right;
+        root.right = left;
+        
+        return root;
+        
+    }
+}
